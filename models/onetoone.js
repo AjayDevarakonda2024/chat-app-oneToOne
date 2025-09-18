24816f593b8cb4fcce1196fd39100ac5175783ac
Ajay Devarakonda <ajaydevarakonda150@gmail.com>
2025-09-18 15:32:56 +0530

    as

--- models/onetoone.js
+++ models/onetoone.js
@@ -4,7 +4,6 @@ const oneToOneSchema = new mongoose.Schema(
     {
         "sender": {type: String },
         "receiver": {type: String},
-        "reciver_message": {type: String},
         "sender_message": {type: String}
     }
 )
